c882deae8e456ba3e7f96887a7ccda4235206030
Andreas Frisch <fraxinas@purplegecko.de>
2021-08-16 18:20:13 +0200

    doorbird: Implement request handler

--- doorbird.py
+++ doorbird.py
@@ -18,6 +18,7 @@
   USA.
 '''
 
+import asyncio
 from aiohttp import web
 from helper import BasePlugin, knxalog as log
 
@@ -27,27 +28,49 @@ def plugin_def():
 class Doorbird(BasePlugin):
     def __init__(self, daemon, cfg):
         super(Doorbird, self).__init__(daemon, cfg)
+        daemon.value_direct_cbs.append(self.process_direct)
         self.doorbird_app = None
         self.doorbird_handler = None
         self.doorbird_server = None
 
     async def handle(self, request):
-        log.debug("handle: {!r}".format(request.rel_url.query))
+        query = request.rel_url.query
+        log.debug(f"{self.device_name} handle: {query!r}")
+        if "toggle" in query:
+            for item in self.obj_list:
+                if "opener" in item:
+                    opener = item
+                if "lock" in item:
+                    lock = item
+            if lock["value"] == "close":
+                await self.d.set_group_value_dict({lock["knx_group"]:"open"})
+                await asyncio.sleep(lock["delay"])
+            await self.d.set_group_value_dict({opener["knx_group"]:"on"})
+            await asyncio.sleep(opener["delay"])
+            await self.d.set_group_value_dict({opener["knx_group"]:"off"})
         return web.Response(text="success\n")
+
+    async def process_direct(self, knx_group, value):
+        try:
+            o = self.get_obj_by_knxgrp(knx_group)
+            log.debug(f"{self.device_name} knx_group={knx_group} value={value}")
+            o["value"] = value
+        except StopIteration:
+            return
 
     def run(self):
         if self.cfg["enabled"]:
             self.doorbird_app = web.Application(debug=True)
             self.doorbird_app.router.add_get('/doorbird/{name}', self.handle)
             self.doorbird_handler = self.doorbird_app.make_handler()
-            log.info("running doorbird endpoint...")
+            log.info(f"{self.device_name} running doorbird endpoint...")
             doorbird_coro = self.d.loop.create_server(self.doorbird_handler, self.d.cfg["sys"]["listenHost"], self.cfg["listenPort"])
             self.doorbird_server = self.d.loop.run_until_complete(doorbird_coro)
             return None
 
     def quit(self):
         if self.doorbird_server:
-            log.info("quit doorbird endpoint...")
+            log.info(f"{self.device_name} quit doorbird endpoint...")
             self.doorbird_server.close()
             self.d.loop.run_until_complete(self.doorbird_app.shutdown())
             self.d.loop.run_until_complete(self.doorbird_handler.shutdown(2.0))
